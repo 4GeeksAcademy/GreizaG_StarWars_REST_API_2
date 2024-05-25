9d40c379de6d04a5db052234f7d1adfeeffdff14
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-05-25 04:39:08 +0000

    [Creación modelo User]

--- src/models.py
+++ src/models.py
@@ -1,19 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
+    
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(32), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return f"User name: {self.name}"
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "last_name": self.last_name,
+            "email": self.email
         }
